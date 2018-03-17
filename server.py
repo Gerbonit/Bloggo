ce87a2b3f197e29e301048a8a89a1a94cd8b3f32
Andrew Andersen <vlasov-andrey.oui@yandex.ru>
2018-03-17 11:53:14 +0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
 
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, make_response
+from flask import Flask, render_template, url_for
 
-app = Flask('Blog')
-
-@app.route('/blank/')
-def return_blank_page():
-  return '\0'
+app = Flask('Bloggo')
 
 @app.route('/user/<username>/')
-def setup_layout(username):
-  return render_template('layout.html', main = '/user/' + username + '/main/')
-
-@app.route('/user/<username>/main/')
 def show_user_profile(username):
   from os import listdir
-  return render_template('/profile.html', user_profile_pic = '/user/' + username + '/profile_picture/',
-                                          username = username,
-                                          userbio = open('users/' + username + '/posts/0', 'rt').read(),
-                                          posts = [ open('users/' + username + '/posts/' + file, 'rt').read()
-                                                    for file in listdir('users/' + username + '/posts') ][1:])
 
-@app.route('/user/<username>/profile_picture/')
-def return_profile_picture(username):
-  from os import listdir
-  items = listdir('users/' + username + '/')
-  for item in items:
-    if item.find('profile_picture') != -1:
-      resp = make_response(open('users/' + username + '/' + item, 'rb').read(), 200)
-      resp.headers['Content-Type'] = 'image/' + item[len('profile_picture') + 1:]
-      return resp
-  return ('', 404)
+  info = eval(open('user/' + username + '/info.json', 'rt').read())
+  name = info['name']
+  surname = info['surname']
+  site = info['site']
+  profile_picture_type = info['profile_picture_type']
+
+  files = [ (int(file), file) for file in listdir('user/' + username + '/post/') ]
+  files.sort()
+  userbio, *posts = [ (pair[1], *open('user/' + username + '/post/' + pair[1], 'rt').read().split('::=', 1)) for pair in files ]
+
+  userbio = userbio[1]
+  posts = posts[::-1]
+  picture = url_for('static', filename = 'profile_picture/' + username + '.' + profile_picture_type)
+
+  return render_template('profile.html', picture  = picture,
+                                         username = username,
+                                         name     = name,
+                                         surname  = surname,
+                                         userbio  = userbio,
+                                         posts    = posts)
 
 if __name__ == '__main__':
-  app.run(debug = True)
+	app.run(debug = True)
